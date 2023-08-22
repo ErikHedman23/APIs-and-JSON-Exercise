0cf496035fac707b8cbf71b837658b6d1d7f06ee
Erik Hedman <erikdavid.hedman21@outlook.com>
2023-08-22 14:42:17 -0400

    Added some notes

--- APIsAndJSON/RonVSKanyeAPI.cs
+++ APIsAndJSON/RonVSKanyeAPI.cs
@@ -25,21 +25,18 @@ namespace APIsAndJSON
 
                 var client_2 = new HttpClient();
 
-
                 var kanyeURL = "https://api.kanye.rest/";
-
 
                 var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
 
-
-                var kanyeQuote = JObject.Parse(kanyeResponse)["quote"].ToString();
+                var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
 
                 var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
 
                 var ronResponse = client_2.GetStringAsync(ronURL).Result;
 
                 var ronQuote = JArray.Parse(ronResponse).First.ToString();
-                //var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim(); -- this is another method to get a similar result.
+                //var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Replace('"', ' ').Trim; -- this is another method to get a similar result.
 
                 Console.Write(". ");
                 Thread.Sleep(500);
